a69915a9fbf1a334bd23fe9824ffbe38253d2cbe
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-06-26 17:05:56 +0200

    First commit

---
+++ src/main/java/com/vanny96/springmvc/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.vanny96.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanny96/springmvc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanny96.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
